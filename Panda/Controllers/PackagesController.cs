c28c8232ced9dc3957b668676df272737bd6947a
ZdravkoBorisov <45686031+ZdravkoBorisov@users.noreply.github.com>
2020-02-13 16:16:07 +0200

    PandaApp
    Implemented Home and Users Controllers (base)

---
+++ Panda/Controllers/PackagesController.cs
@@ -0,0 +1,31 @@
+﻿using SIS.HTTP;
+using SIS.MvcFramework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Panda.Controllers
+{
+   public class PackagesController : Controller
+    {
+        public HttpResponse Details()
+        {
+            return this.View();
+        }
+
+        public HttpResponse Create()
+        {
+            return this.View();
+        }
+
+        public HttpResponse Delivered()
+        {
+            return this.View();
+        }
+
+        public HttpResponse Pending()
+        {
+            return this.View();
+        }
+    }
+}
---
+++ Panda/Services/IUsersService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Panda.Services
+{
+    public interface IUsersService
+    {
+        void CreateUser(string username, string email,string password);
+
+        string GetUserId(string username, string password);
+
+        string GetUsername(string id);
+
+        bool IsUsernameUsed(string username);
+
+        bool IsEmailUsed(string email);
+
+    }
+}
